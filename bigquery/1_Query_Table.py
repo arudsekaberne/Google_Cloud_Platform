14785e008ba8aa360def2559537067366ed1cc40
arudsekaberne <68807002+arudsekaberne@users.noreply.github.com>
2023-11-26 10:03:32 +0530

    Update 1_Query_Table.py

--- bigquery/1_Query_Table.py
+++ bigquery/1_Query_Table.py
@@ -11,15 +11,15 @@ client = bigquery.Client()
 
 
 # 2. Write a query
-select_query = "SELECT * FROM <table_identitifer> LIMIT 10;"
+select_query = f"SELECT * FROM <table_identifier> LIMIT 2;"
 
 
 # 3. Make an API request
-query_response = client.query(select_query)
+query_job = client.query(select_query)
 
 
 # 4. Iterator
-for row in query_response.result():
+for row in query_job.result():
 
     # Use index position (or) dot notation to access column
     print(row[0], row.account_number)
